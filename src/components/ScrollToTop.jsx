ee79d6a049194754c46d6c18a6bc88a51c1e4063
CF HUB UK <info@cfhubuk.co.uk>
2026-04-06 12:57:13 +0300

    Fix: scroll to top instantly and make banner images less zoomed in

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom'
 function ScrollToTop() {
   const { pathname } = useLocation()
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+    window.scrollTo(0, 0)
   }, [pathname])
   return null
 }
